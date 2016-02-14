f1df2cf445ff43ccbbc3eae9371c23a7f4f2e33e
ericKuik <eric.kuik@duo.nl>
2016-02-14 19:47:15 +0100

    initial

---
+++ app/src/main/java/tekstbureauexlibris/akwardapp/MainActivity.java
@@ -0,0 +1,85 @@
+package tekstbureauexlibris.akwardapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+import android.content.Intent;
+import android.view.Display;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewFlipper viewFlipper;
+    private float lastX;
+
+    @Override protected void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+       //setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_theme1);
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+    }
+
+   // public void gototheme1(View view) {
+        //setContentView(R.layout.activity_theme1);
+
+    //    Intent intent = new Intent(this, activity_theme1.class);
+
+  //  }
+
+
+
+
+
+    // Method to handle touch event like left to right swap and right to left swap
+    public boolean onTouchEvent(MotionEvent touchevent)
+    {
+        switch (touchevent.getAction())
+        {
+            // when user first touches the screen to swap
+            case MotionEvent.ACTION_DOWN:
+            {
+                lastX = touchevent.getX();
+                break;
+            }
+            case MotionEvent.ACTION_UP:
+            {
+                float currentX = touchevent.getX();
+
+                // if left to right swipe on screen
+                if (lastX < currentX)
+                {
+                    // If no more View/Child to flip
+                    if (viewFlipper.getDisplayedChild() == 0)
+                        break;
+
+                    // set the required Animation type to ViewFlipper
+                    // The Next screen will come in form Left and current Screen will go OUT from Right
+                    viewFlipper.setInAnimation(this, R.anim.in_from_left);
+                    viewFlipper.setOutAnimation(this, R.anim.out_to_right);
+                    // Show the next Screen
+                    viewFlipper.showNext();
+                }
+
+                // if right to left swipe on screen
+                if (lastX > currentX)
+                {
+                    if (viewFlipper.getDisplayedChild() == 1)
+                        break;
+                    // set the required Animation type to ViewFlipper
+                    // The Next screen will come in form Right and current Screen will go OUT from Left
+                    viewFlipper.setInAnimation(this, R.anim.in_from_right);
+                    viewFlipper.setOutAnimation(this, R.anim.out_to_left);
+                    // Show The Previous Screen
+                    viewFlipper.showPrevious();
+                }
+                break;
+            }
+        }
+        return false;
+    }
+
+}
